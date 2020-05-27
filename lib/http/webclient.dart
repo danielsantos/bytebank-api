bebd1c16ccab87bacbe96f5037a52984f1ee5dbf
Daniel Rocha <daniel.roch@riocardmais.com.br>
2020-05-27 15:39:05 -0300

    implementing save/post transaction

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -5,13 +5,15 @@ import 'package:bytebank_sqflite/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-Future<List<Transaction>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(interceptors: [
-    LoggingInterceptor(),
-  ]);
+final Client client = HttpClientWithInterceptor.build(interceptors: [
+  LoggingInterceptor(),
+]);
 
+const String baseUrl = 'http://192.168.1.4:8080/transactions';
+
+Future<List<Transaction>> findAll() async {
   final Response response =
-      await client.get('http://192.168.1.4:8080/transactions').timeout(Duration(seconds: 5));
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
 
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
@@ -32,7 +34,6 @@ Future<List<Transaction>> findAll() async {
   }
 
   return transactions;
-
 }
 
 class LoggingInterceptor implements InterceptorContract {
@@ -53,4 +54,38 @@ class LoggingInterceptor implements InterceptorContract {
     print('body: ${data.body}');
     return data;
   }
+}
+
+Future<Transaction> save(Transaction transaction) async {
+
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(
+    baseUrl,
+    headers: {'Content-type': 'application/json', 'password': '1000'},
+    body: transactionJson,
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+
+  final Transaction transactionCreated = Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
+
+  return transactionCreated;
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:bytebank_sqflite/http/webclient.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/dashboard.dart';
 
 void main() {
   runApp(BytebankApp());
-  //save(Contact(0, 'teste', 1234));
-  findAll().then((transcations) => print('All Transaction: $transcations'));
+  //save(Contact(0, 'teste', 1234)); // sqflite
+  // web-api //findAll().then((transcations) => print('All Transaction: $transcations'));
+  // web-api //save(Transaction(235.0, Contact(0, 'Siclano', 2345))).then((transaction) => print(transaction));
 }
 
 class BytebankApp extends StatelessWidget {
